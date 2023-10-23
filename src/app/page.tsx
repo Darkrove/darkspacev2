f209826d515282234c0097dd4b85c265681b52db
sajjad shaikh <53792139+Darkrove@users.noreply.github.com>
2023-10-23 08:44:47 +0530

    update padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Page() {
           </span>
         </p>
         {/* <Downloads /> */}
-	      <Link target='_blank' href="https://www.github.com/darkrove/darkspacev2" className="home-button-border-gradient relative z-30 flex cursor-pointer items-center gap-2 !rounded-[7px] border-0 !bg-[#2F3152]/30 py-2 text-sm text-white !backdrop-blur-lg hover:brightness-110 md:text-[16px]">
+	      <Link target='_blank' href="https://www.github.com/darkrove/darkspacev2" className="home-button-border-gradient relative z-30 flex cursor-pointer items-center gap-2 !rounded-[7px] border-0 !bg-[#2F3152]/30 py-2 px-3 text-sm text-white !backdrop-blur-lg hover:brightness-110 md:text-[16px]">
             <span className="flex items-center justify-center space-x-2">
              <Icons.gitHub className="h-4 w-4" />
 		    
