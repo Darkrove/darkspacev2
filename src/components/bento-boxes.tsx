6b20a1d0f792eed41cc0857229640a740d76914b
sajjad <samaralishaikh212@gmail.com>
2023-10-24 19:04:42 +0530

    updated padding

--- src/components/bento-boxes.tsx
+++ src/components/bento-boxes.tsx
@@ -7,7 +7,7 @@ interface Props {}
 
 const BentoBoxes = () => {
   return (
-    <div className="z-40  w-full p-10">
+    <div className="z-40  w-full py-10 md:p-10">
       <LargeHeading className="mb-5">
         Own your data for the first time
       </LargeHeading>
